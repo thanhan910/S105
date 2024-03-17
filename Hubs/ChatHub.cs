d4a7f540901eb58409c9b0762c616669ada418ff
thanhan910 <63280785+thanhan910@users.noreply.github.com>
2024-03-18 02:20:30 +1100

    Refactor ChatHub to use NewMessage method

--- Hubs/ChatHub.cs
+++ Hubs/ChatHub.cs
@@ -1,12 +1,9 @@
 using Microsoft.AspNetCore.SignalR;
 
-namespace S105.Hubs
+namespace S105.Hubs;
+
+public class ChatHub : Hub
 {
-    public class ChatHub : Hub
-    {
-        public async Task SendMessage(string user, string message)
-        {
-            await Clients.All.SendAsync("ReceiveMessage", user, message);
-        }
-    }
+    public async Task NewMessage(long username, string message) =>
+        await Clients.All.SendAsync("messageReceived", username, message);
 }
