5770f454f91cf337e85ca320d3a52309d9aa5ecd
thanhan910 <63280785+thanhan910@users.noreply.github.com>
2024-04-11 12:24:15 +1000

    Add new test for testing a specific endpoint

--- Tests/TestAPI.cs
+++ Tests/TestAPI.cs
@@ -71,5 +71,28 @@ namespace S105.Hubs
             // Assert that result is not null
             Assert.IsNotNull(result);
         }
+        
+        [TestMethod]
+        public async Task TestGetDataAsync2()
+        {
+            // Get API keys from json file local-env.json
+            var json = File.ReadAllText("local-env.json");
+            var values = JsonSerializer.Deserialize<JsonElement>(json);
+            string apiKey = values.GetProperty("PTV_TIMETABLE_API_KEY").GetString() ?? "";
+            string developerId = values.GetProperty("PTV_TIMETABLE_DEV_ID").GetString() ?? "";
+
+            // Convert to int
+            int devId = Int32.Parse(developerId);
+
+            var httpClient = new HttpClient();
+
+            var client = new PTVAPI3(apiKey, devId, httpClient);
+            var result = await client.GetDataAsync("/v3/departures/route_type/2/stop/22753?max_results=20&include_cancelled=true&look_backwards=true&expand=All");
+            
+            Console.WriteLine(result);
+            
+            // Assert that result is not null
+            Assert.IsNotNull(result);
+        }
     }
 }
