ca5523e4d126b249199bf028f785da0a04151960
thanhan910 <63280785+thanhan910@users.noreply.github.com>
2024-04-12 16:32:35 +1000

    Wrap Program code inside the Main function to avoid polluting the global namespace

--- Program.cs
+++ Program.cs
@@ -1,31 +1,38 @@
 using S105.Hubs;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddHttpClient();
-builder.Services.AddRazorPages();
-builder.Services.AddSignalR();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+public class Program
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddHttpClient();
+        builder.Services.AddRazorPages();
+        builder.Services.AddSignalR();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseDefaultFiles();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.MapRazorPages();
+        app.MapHub<ChatHub>("/chatHub");
+
+        app.Run();
+        
+    }
 }
-
-app.UseHttpsRedirection();
-app.UseDefaultFiles();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapRazorPages();
-app.MapHub<ChatHub>("/chatHub");
-
-app.Run();
